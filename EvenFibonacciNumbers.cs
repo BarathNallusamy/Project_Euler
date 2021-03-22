9cc702b85f6057381811bcf3c24b2fd44556792c
baray7 <nallu.baray@gmail.com>
2021-03-22 17:01:47 +0000

    Add the first two test cases and solutions

---
+++ EvenFibonacciNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    public class EvenFibonacciNumbers
+    {
+
+        public static (int number, int eventotal) HighestNumberOfFibonacciSeries(int x)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 0;
+            int sum = 0, num = 0;
+            //int results = 4000000;
+
+            for (int i = 0; i < x - 2; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                if (a % 2 == 0)
+                {
+                    sum += a;
+                }
+            }
+            return (num, sum);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,11 +2,53 @@
 
 namespace Project_Euler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+        }
+
+        public static int MultiplesOfThreeAndFive(int x)
+        {
+            int sum = 0;
+            for (int i = 0; i < x; i++)
+            {
+                if (i% 3 == 0)
+                {
+                     sum += i;
+                }
+                else if(i%5==0)
+                {
+                    sum+=i;
+                }
+            }
+            return sum;
+        }
+
+        public static int LargestPalindromeNumber(int x)
+        {
+            
+            int a = 9, b = 9;
+            int sum = 0, reverse = 0;
+
+            for (int i = 9; i > 0; i--)
+            {
+                sum = a * b;
+
+                while (sum > 0)
+                {
+                    int remainder = sum % 10;
+                    reverse = (reverse * 10) + remainder;
+                    sum = sum / 10;
+                }
+
+            }
+            Console.WriteLine(sum + "" + reverse);
+            return reverse;
+
+
         }
     }
 }
